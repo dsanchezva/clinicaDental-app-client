db46e3f9248e159dee5dd36719663fd5121d6ed4
David Sanchez Valera <chirlo14@gmail.com>
2024-02-02 16:52:45 +0100

    deploy problem solved

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -6,18 +6,18 @@ function TeamCard(props) {
   const {isLoggedIn} = useContext(AuthContext)
 
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <img class="w-full " src={imgUrl} alt="Foto miembro equipo" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">{name}</div>
-        <p class="text-gray-700 text-base">{description}</p>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img className="w-full " src={imgUrl} alt="Foto miembro equipo" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{name}</div>
+        <p className="text-gray-700 text-base">{description}</p>
       </div>
       {isLoggedIn ?    
       <div className="flex flex-row gap-10 justify-center mb-5">
-        <button class="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded-full">
+        <button className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded-full">
           Borrar
         </button>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
           Editar
         </button>
       </div>
