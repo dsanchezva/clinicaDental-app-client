dbc25d92282e42a1564b6a6d516ca35c192ab4dc
David Sanchez Valera <chirlo14@gmail.com>
2024-02-06 15:49:39 +0100

    background

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -24,7 +24,7 @@ try {
   }
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-slate-200">
       <img className="w-full" src={imgUrl} alt="Foto miembro equipo" />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{name}</div>
